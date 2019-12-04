6fe7d7b630706f9424e78e65e52550b0b0a4d4e5
Saurabh Banore <saurabh.banore@townscript.com>
2019-12-05 00:09:00 +0530

    added test

--- src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
+++ src/test/java/co/interleap/courses/tdd/VowelCounterTest.java
@@ -1,9 +1,16 @@
 package co.interleap.courses.tdd;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class VowelCounterTest {
 
     @Test
     public void dummy(){}
+    
+    @Test 
+    public void shouldCountZeroWithAnEmptyString(){
+        assertEquals(0, new VowelCounter().count(""));
+    }
 }
